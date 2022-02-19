c1071a8839ca427f6730e64ef75c1b0c60e95599
Damon Achey <damon@achey.net>
2022-02-19 16:05:22 -0700

    store settings in registry and reload on startup

--- SystemMonitorTray/Tray.cs
+++ SystemMonitorTray/Tray.cs
@@ -1,12 +1,22 @@
+using Microsoft.Win32;
+
 using Networking;
+
+using System.Reflection;
 
 namespace SystemMonitorTray;
 
 public partial class Tray : Form
 {
+    private readonly string appKey = @"Software\Achey\SystemMonitor";
+    private readonly string runKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+    private readonly string runName = @"Achey\SystemMonitor";
+
     private readonly INetworkMonitor networkMonitor;
     private readonly NotifyIcon trayIcon;
+    
     private bool sound = true;
+    private bool startWithWindows = false;
 
     public Tray(INetworkMonitor networkMonitor)
     {
@@ -21,8 +31,35 @@ public partial class Tray : Form
             Visible = true,
         };
 
+        InitializeOptions();
+
         // TODO: remove after debugging
         new Details(networkMonitor).Show();
+    }
+
+    private void InitializeOptions()
+    {
+        var key = Registry.CurrentUser.CreateSubKey(appKey);
+        sound = bool.Parse((string)key.GetValue("Sound", true.ToString())!);
+        
+        key = Registry.CurrentUser.CreateSubKey(runKey);
+        startWithWindows = key.GetValue(runName) != null;
+
+        foreach (object item in trayIcon.ContextMenuStrip.Items)
+        {
+            if (item is ToolStripMenuItem menuItem)
+            {
+                if (menuItem.Text == "Start With Windows")
+                {
+                    menuItem.Checked = startWithWindows;
+                }
+
+                if (menuItem.Text == "Sound")
+                {
+                    menuItem.Checked = sound;
+                }
+            }
+        }
     }
 
     private ContextMenuStrip GetContextMenu()
@@ -32,15 +69,18 @@ public partial class Tray : Form
         var detailsMenuItem = new ToolStripMenuItem("Details");
         var settingsMenuItem = new ToolStripMenuItem("Settings");
         var soundMenuItem = new ToolStripMenuItem("Sound") { Checked = true, CheckOnClick = true };
+        var startWithWindowsItem = new ToolStripMenuItem("Start With Windows") { Checked = false, CheckOnClick = true };
 
         detailsMenuItem.Click += OnDetails;
         settingsMenuItem.Click += OnSettings;
         soundMenuItem.Click += OnSound;
+        startWithWindowsItem.Click += OnStartWithWindows;
 
         contextMenuStrip.Items.Add(detailsMenuItem);
         contextMenuStrip.Items.Add("-");
         contextMenuStrip.Items.Add(settingsMenuItem);
         contextMenuStrip.Items.Add(soundMenuItem);
+        contextMenuStrip.Items.Add(startWithWindowsItem);
         contextMenuStrip.Items.Add("-");
         contextMenuStrip.Items.Add("Exit", null, OnExit);
 
@@ -89,10 +129,36 @@ public partial class Tray : Form
 
     private void OnSound(object? sender, EventArgs e)
     {
-        if (sender is ToolStripMenuItem soundMenuItem)
-            sound = soundMenuItem?.Checked ?? false;
+        if (sender is ToolStripMenuItem item)
+        {
+            sound = item?.Checked ?? false;
+        }
 
-        if (sound) Console.Beep();
+        var key = Registry.CurrentUser.CreateSubKey(appKey);
+        key.SetValue("Sound", sound.ToString());
+
+        if (sound)
+        {
+            Console.Beep();
+        }
+    }
+
+    private void OnStartWithWindows(object? sender, EventArgs e)
+    {
+        if (sender is ToolStripMenuItem item)
+        {
+            startWithWindows = item?.Checked ?? false;
+        }
+
+        var key = Registry.CurrentUser.CreateSubKey(runKey);
+        if (startWithWindows)
+        {
+            key.SetValue(runName, Assembly.GetEntryAssembly()?.Location!);
+        }
+        else
+        {
+            key.DeleteValue(runName);
+        }
     }
 
     private void OnExit(object? sender, EventArgs e)
