de2d4a31949701dd7808efcb4b65dba23817ec53
Damon Achey <damon@achey.net>
2022-02-22 20:45:03 -0700

    set defaults for settings screen

--- SystemMonitorTray/SettingsForm.cs
+++ SystemMonitorTray/SettingsForm.cs
@@ -4,6 +4,20 @@ public partial class SettingsForm : Form
 {
     public SettingsForm()
     {
+        BackColor = Properties.Settings.Default.applicationBackgroundColor;
+        ForeColor = Properties.Settings.Default.applicationForegroundColor;
+        Location = Properties.Settings.Default.settingsFormLocation;
+        Size = Properties.Settings.Default.settingsFormSize;
+        StartPosition = FormStartPosition.Manual;
+
         InitializeComponent();
+
+        FormClosing += (o, e) => OnFormClosing();
+    }
+
+    private void OnFormClosing()
+    {
+        Properties.Settings.Default.settingsFormLocation = Location;
+        Properties.Settings.Default.settingsFormSize = Size;
     }
 }
