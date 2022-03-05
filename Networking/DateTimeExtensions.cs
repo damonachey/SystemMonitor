ea78bb02d2f3630ca6ba1da2cea9cd5aad03d64b
Damon Achey <damon@achey.net>
2022-03-04 22:12:44 -0700

    update new syntax

--- Networking/DateTimeExtensions.cs
+++ Networking/DateTimeExtensions.cs
@@ -4,12 +4,12 @@ public static class DateTimeExtensions
 {
     public static DateTime RoundUp(this DateTime dt, TimeSpan d)
     {
-        return new DateTime((dt.Ticks + d.Ticks - 1) / d.Ticks * d.Ticks, dt.Kind);
+        return new((dt.Ticks + d.Ticks - 1) / d.Ticks * d.Ticks, dt.Kind);
     }
 
     public static DateTime RoundDown(this DateTime dt, TimeSpan d)
     {
-        return new DateTime(dt.Ticks / d.Ticks * d.Ticks, dt.Kind);
+        return new(dt.Ticks / d.Ticks * d.Ticks, dt.Kind);
     }
 
     public static DateTime StartOfWeek(this DateTime dt)
@@ -19,6 +19,6 @@ public static class DateTimeExtensions
 
     public static DateTime StartOfMonth(this DateTime dt)
     {
-        return new DateTime(dt.Year, dt.Month, 1);
+        return new(dt.Year, dt.Month, 1);
     }
 }
