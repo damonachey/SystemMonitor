fb761b4d6a8a48e4a8c59dd238b1e60d108cc47e
Damon Achey <damon@achey.net>
2022-03-04 22:56:49 -0700

    fix variable name

--- Networking/Delay.cs
+++ Networking/Delay.cs
@@ -5,8 +5,8 @@ internal class Delay
     public static async Task Next(TimeSpan interval)
     {
         var now = DateTime.Now;
-        var nextMinute = now.RoundUp(interval) - now;
+        var nextInterval = now.RoundUp(interval) - now;
 
-        await Task.Delay(nextMinute);
+        await Task.Delay(nextInterval);
     }
 }
