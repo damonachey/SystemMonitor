ce319d5e3e99bf710e0b785e68bad27457fc31fc
Damon Achey <damon@achey.net>
2022-03-05 23:54:36 -0700

    update version to 0.3.0

--- SystemMonitor/publish.cmd
+++ SystemMonitor/publish.cmd
@@ -1,5 +1,5 @@
 REM From https://dotnetcoretutorials.com/2021/11/10/single-file-apps-in-net-6/
 
-dotnet publish -p:PublishSingleFile=true -r win-x64 -c Release --self-contained true
+dotnet publish -p:PublishSingleFile=true -r win-x64 -c Release --self-contained true /p:AssemblyVersion=0.3.0 /p:Version=0.3.0
 
 start bin\Release\net6.0-windows\win-x64\publish
