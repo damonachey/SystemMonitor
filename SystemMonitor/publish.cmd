273921f651cd28fb1decff99488ba25efbc65127
Damon Achey <damon@achey.net>
2023-06-26 20:16:13 -0600

    Update version

--- SystemMonitor/publish.cmd
+++ SystemMonitor/publish.cmd
@@ -1,5 +1,5 @@
 REM From https://dotnetcoretutorials.com/2021/11/10/single-file-apps-in-net-6/
 
-dotnet publish -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true -r win-x64 -c Release --self-contained true /p:AssemblyVersion=0.3.1 /p:Version=0.3.1
+dotnet publish -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true -r win-x64 -c Release --self-contained true /p:AssemblyVersion=0.3.2 /p:Version=0.3.2
 
 start bin\Release\net6.0-windows\win-x64\publish
